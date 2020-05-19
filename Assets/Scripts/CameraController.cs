1e521dacca30709b0dfea66ae0825a1b8f6c397c
Elijah <enutter24@gmail.com>
2020-05-18 18:21:17 -0700

    CameraPart1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //declaring the different variables needed
+    public GameObject player;
+    private Vector3 offset;
+
+    //sets the position of the camera
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    //moves the camera with the player
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
