4716f8f93c6b8e4a690a8878e62e98c155f313aa
Elijah <enutter24@gmail.com>
2020-05-20 13:50:40 -0700

    Variables

--- Assets/Scripts/TankData.cs
+++ Assets/Scripts/TankData.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class TankData : MonoBehaviour
 {
-    [Header ("Speed")]
+    [Header("Movement")]
     public float moveSpeedForward = 5.0f;
     public float moveSpeedBack = 2.0f;
     public float rotateSpeed = 90.0f;
+
+    [Header("Shells")]
+    public float shellForce = 10.0f;
+    public float damageDone = 25.0f;
+    public float fireRate = 3.0f;
+
+    [Header("Health")]
+    public float maxHealth = 200.0f;
+
+    [Header("Score")]
+    public int score = 0;
 }
